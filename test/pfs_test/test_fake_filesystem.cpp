5a263d98ba620875bd7e0bac6e5c30a6bfb7c297
Sam Warring <samwarring@gmail.com>
2023-01-28 16:29:47 -0800

    Add vcpkg, catch2, and presets

---
+++ test/pfs_test/test_fake_filesystem.cpp
@@ -0,0 +1,4 @@
+#include <catch2/catch_test_macros.hpp>
+#include <pfs/fake_filesystem.hpp>
+
+TEST_CASE("fake_filesystem") { pfs::fake_filesystem fs; }
---
+++ test/pfs_test/test_std_filesystem.cpp
@@ -0,0 +1,4 @@
+#include <catch2/catch_test_macros.hpp>
+#include <pfs/std_filesystem.hpp>
+
+TEST_CASE("std_filesystem") { pfs::std_filesystem fs; }
